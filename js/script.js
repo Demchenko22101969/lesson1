1fbdf5554f65b4710acc56b7c495b369cb9ca747
sergiy-kyiv-2022 <s22101969@gmail.com>
2023-07-12 13:53:20 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+/*const storeName = "Silpo";
+const storeDescription = {
+    budget : 10000,
+    employees : ["Lesia","Andriy","Oliy"],
+    products : {
+        potatis:50,
+        tomato:170,
+    },
+    open:true,
+}*/
+//alert("Hello, World");
+//const result = confirm("Are You Here?");
+//console.log(result);
+//const answer = prompt("Are You 18?", "18");
+//console.log(answer);
+//const answer = +prompt("Are You 18?", "18")
+//console.log(typeof(answer));
+//const answers = [];
+//answers[0] = prompt("What is Your First name?", "");
+//answers[1] = prompt("What is Your Last name?", "");
+//answers[2] = prompt("How old are You?", "");
+//document.write(answers);
+//console.log(typeof(answers));
+
+//Operators
+
+let incr = 10,
+    decr = 10;
+incr++; //postfix form
+decr--; //postfix form
+++incr;//prefix form
+--decr;//prefix form
+console.log(incr);
+console.log(decr);
